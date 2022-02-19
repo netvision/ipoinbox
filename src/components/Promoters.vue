489f0ce332746681a5ebd8ce5fdaec1ecea18f23
Rakesh Jangid <rakesh@jangid.co.in>
2022-02-19 16:47:14 +0530

    frontend ui trial

---
+++ src/components/Promoters.vue
@@ -0,0 +1,46 @@
+/* eslint-disable vue/valid-template-root */
+<template>
+<q-card flat id="promoters" class="q-mt-md rounded-border bg-orange-1">
+    <q-card-section>
+    <div class="text-h5">Promoters</div>
+    </q-card-section>
+    <q-separator />
+    <q-card-section horizontal class="q-pa-md q-gutter-md">
+    <q-card v-for="promoter in promoters" :key="promoter.id" flat bordered>
+            <q-card-section horizontal>
+                <q-card-section class="col-5 flex items-start">
+                <q-img
+                    class="rounded-borders"
+                    :src="promoter.photo"
+                />
+                <div class="q-pa-md">
+                    <p class="text-subtitle1">Pre-offer Shareholding</p>
+                    <p class="text-overline">{{promoter.pre_offer_shares}} ({{promoter.pre_offer_percentage}}%)</p>
+                    <q-separator spaced />
+                    <p class="text-subtitle1">Post-offer Shareholding</p>
+                    <p class="text-overline">{{promoter.post_offer_shares}} ({{promoter.post_offer_percentage}}%)</p>
+                </div>
+                </q-card-section>
+                <q-card-section class="q-pt-xs">
+                <div class="text-overline">{{promoter.type}}</div>
+                <div class="text-h5 q-mt-sm q-mb-xs">{{promoter.name}}</div>
+                <div class="text-caption text-grey" v-html="promoter.description"></div>
+                </q-card-section>
+            </q-card-section>
+        </q-card>
+    </q-card-section>
+</q-card>
+</template>
+<script setup>
+import { defineProps, ref, onMounted  } from 'vue'
+import { axios } from '../boot/axios'
+const props = defineProps({
+    ipo_id: Number
+  })
+console.log(props.ipo_id)
+const promoters = ref([])
+onMounted(async() => {
+    promoters.value = await axios.get('https://droplet.netserve.in/promoters?ipo_id='+props.ipo_id).then(r => r.data)
+    console.log(promoters.value)
+})
+</script>
--- src/pages/Ipo.vue
+++ src/pages/Ipo.vue
@@ -1,29 +1,17 @@
 <template>
 <q-ajax-bar ref="bar" position="top" color="accent" size="10px" />
   <q-page class="q-pa-md">
-     <div class="q-mb-md" :style="'background-image: url(\''+ipo.header_img+'\'); height:300px; background-size:cover'">
+     <div :style="'background-image: url(\''+ipo.header_img+'\'); height:300px; background-size:cover'">
           <img :src="ipo.company_logo" style="width: 150px; height: 150px; position: absolute; top: 50px; left: 50px">
       </div>
-    
-    <div class="text-h3 text-deep-orange-6 text-left q-ma-md">{{ipo.company_name}}</div>    
-    <div class="q-pa-sm q-ml-sm flex q-gutter-sm bg-grey-3 rounded-borders">
-      <span><a :href="ipo.company_url" target="_blank">{{ipo.company_url}}</a></span> <q-separator color="orange" vertical /> <a :href="nse.url">NSE</a> <q-separator color="orange" vertical /> <a :href="bse.url">BSE</a><q-separator color="orange" vertical /><span> Registrar: <a :href="registrar.url">{{registrar.name}}</a></span>
-    </div>
-    <div class="q-pa-md">
     <q-card flat class="rounded-border bg-orange-1">
-    <q-item>
-        <q-item-section avatar>
-          <q-avatar square rounded>
-            <img :src="ipo.company_logo">
-          </q-avatar>
-        </q-item-section>
-        <q-item-section>
-          <q-item-label header class="text-h4 text-deep-orange-6">Overview</q-item-label>
-        </q-item-section>
-      </q-item>
       <q-card-section>
-      <div class="row q-gutter-sm">
-        <div class="col rounded-border">
+      <div class="text-h3 text-deep-orange-6 text-left q-pb-md">{{ipo.company_name}}</div> 
+      <div class="q-pa-sm flex q-gutter-sm bg-orange-3 rounded-borders">
+        <span><a :href="ipo.company_url" target="_blank">{{ipo.company_url}}</a></span> <q-separator color="orange" vertical /> <a :href="nse.url">NSE</a> <q-separator color="orange" vertical /> <a :href="bse.url">BSE</a><q-separator color="orange" vertical /><span> Registrar: <a :href="registrar.url">{{registrar.name}}</a></span>
+      </div>
+      <div class="row q-gutter-sm q-pa-md justify-between item-stretch">
+        <div class="rounded-border col-grow">
           <q-item>
             <q-item-section>
               <q-item-label overline>Sector</q-item-label>
@@ -63,14 +51,14 @@
           </q-item>
         </div>
         <q-separator vertical spaced color="orange" />
-        <div class="col">
+        <div class="col-grow">
           <q-item>
             <q-item-section>
               <q-item-label overline>Listing Date</q-item-label>
               <q-item-label class="text-bold">{{date.formatDate(nse.listing_date, 'DD-MM-YYYY')}}</q-item-label>
             </q-item-section>
             <q-item-section>
-              <q-item-label overline>Isssue Price</q-item-label>
+              <q-item-label overline>Issue Price</q-item-label>
               <q-item-label class="text-bold">{{ipo.issue_price}}</q-item-label>
             </q-item-section>
           </q-item>
@@ -109,16 +97,20 @@
             </q-item-section>
             <q-item-section>
               <q-item-label overline>Free Float</q-item-label>
-              <q-item-label class="text-bold">{{nse.free_float}}</q-item-label>
+              <q-item-label class="text-bold">{{(nse.free_float) ? nse.free_float : 'NA'}}</q-item-label>
             </q-item-section>
           </q-item>
         </div>
        <q-separator vertical spaced color="orange" />
-        <div class="col">
+        <div class="col-grow">
           <q-item>
             <q-item-section>
               <q-item-label overline>EOD on</q-item-label>
               <q-item-label class="text-bold">{{date.formatDate(eod.date, 'DD-MM-YYYY')}}</q-item-label>
+            </q-item-section>
+            <q-item-section>
+              <q-item-label overline>Open</q-item-label>
+              <q-item-label class="text-bold">{{eod.Open}}</q-item-label>
             </q-item-section>
             <q-item-section>
               <q-item-label overline>Closing</q-item-label>
@@ -127,10 +119,6 @@
           </q-item>
           <q-item>
             <q-item-section>
-              <q-item-label overline>Open</q-item-label>
-              <q-item-label class="text-bold">{{eod.Open}}</q-item-label>
-            </q-item-section>
-            <q-item-section>
               <q-item-label overline>Day High</q-item-label>
               <q-item-label class="text-bold">{{eod.High}}</q-item-label>
             </q-item-section>
@@ -138,53 +126,59 @@
               <q-item-label overline>Day Low</q-item-label>
               <q-item-label class="text-bold">{{eod.Low}}</q-item-label>
             </q-item-section>
-          </q-item>
-          <q-item>
             <q-item-section>
-              <q-item-label overline>Volume of the Day</q-item-label>
+              <q-item-label overline>Volume</q-item-label>
               <q-item-label class="text-bold">{{eod.Volume}}</q-item-label>
-              <q-item-label caption class="text-bold">of Value &#8377;{{eod.Value}}</q-item-label>
-            </q-item-section>
-            <q-item-section>
-              <q-item-label overline>Market Cap</q-item-label>
-              <q-item-label class="text-bold">&#8377;{{eod.mcap}} Cr.</q-item-label>
-              <q-item-label caption class="text-bold">as on {{date.formatDate(eod.date, 'DD-MM-YYYY')}}</q-item-label>
+              <q-item-label caption class="text-bold">&#8377;{{eod.Value}}</q-item-label>
             </q-item-section>
           </q-item>
           <q-item>
             <q-item-section>
               <q-item-label overline>Lifetime High</q-item-label>
               <q-item-label class="text-bold">{{eod.LifeTimeHigh}}</q-item-label>
-              <q-item-label caption class="text-bold">on {{date.formatDate(eod.highdate, 'DD-MM-YYYY')}}</q-item-label>
+              <q-item-label caption class="text-bold text-italic">on {{date.formatDate(eod.highdate, 'DD-MM-YYYY')}}</q-item-label>
             </q-item-section>
             <q-item-section>
               <q-item-label overline>Lifetime Low</q-item-label>
               <q-item-label class="text-bold">{{eod.LifeTimeLow}}</q-item-label>
-              <q-item-label caption class="text-bold">on {{date.formatDate(eod.lowdate, 'DD-MM-YYYY')}}</q-item-label>
+              <q-item-label caption class="text-bold text-italic">on {{date.formatDate(eod.lowdate, 'DD-MM-YYYY')}}</q-item-label>
+            </q-item-section>
+            <q-item-section>
+              <q-item-label overline>Market Cap</q-item-label>
+              <q-item-label class="text-bold">&#8377;{{eod.mcap}} Cr.</q-item-label>
+              <q-item-label caption class="text-bold text-italic">as on {{date.formatDate(eod.date, 'DD-MM-YYYY')}}</q-item-label>
             </q-item-section>
           </q-item> 
         </div>
       </div>
-      
       </q-card-section>
     </q-card>
-    </div>
-    <q-space />
-    
+    <q-card flat class="q-mt-md rounded-border bg-orange-1">
+      <q-card-section>
+        <div class="text-h5">Company Info</div>
+        <div v-html="ipo.about_html" />
+      </q-card-section>
+    </q-card>
+    <Promoters :ipo_id="ipo_id" />
     <pre>{{ipo}}</pre>
     <pre>{{curInfo}}</pre>
     <pre>{{eod}}</pre>
-
+    <q-page-scroller expand position="top" :scroll-offset="450" :offset="[0, 0]">
+      <div class="q-pa-sm flex q-gutter-sm bg-orange-3 rounded-borders full-width">
+        <span>{{ipo.company_name}}</span> <q-space /><q-separator color="orange" vertical /> <a @click="scrollTo('promoters')">Promoters</a> <q-separator color="orange" vertical /> <a :href="bse.url">BSE</a><q-separator color="orange" vertical /><span> Registrar: <a :href="registrar.url">{{registrar.name}}</a></span>
+      </div>
+    </q-page-scroller>
   </q-page>
 </template>
 
 <script setup>
-  import { ref, onMounted } from 'vue'
+  import { ref, onBeforeMount } from 'vue'
   import { useRoute } from 'vue-router'
   import { axios } from '../boot/axios'
-  import { useMeta } from 'quasar'
+  import { useMeta, scroll } from 'quasar'
   import { date } from 'quasar'
-  
+  import Promoters from '../components/Promoters.vue'
+  const { getScrollTarget, setVerticalScrollPosition } = scroll
   const route = useRoute()
   const ipo_id = route.params.ipo_id
   const ipo = ref({})
@@ -193,7 +187,16 @@
   const registrar = ref({})
   const eod = ref({})
   const curInfo = ref({})
-  onMounted(async() => {
+
+  const scrollTo = (el) => {
+    const ele = document.getElementById(el)
+    const target = getScrollTarget(ele)
+    const offset = ele.offsetTop - ele.scrollHeight
+    console.log(offset)
+    const duration = 1000
+    setVerticalScrollPosition(target, offset, duration)
+  }
+  onBeforeMount(async() => {
     ipo.value = await axios.get('https://droplet.netserve.in/ipos/'+ipo_id+'?expand=registrar,promoters,listings,subscriptions,sector').then(r => r.data)
     let listings = ipo.value.listings
     nse.value = listings.filter(r => r.exchange == 'NSE')[0]
@@ -207,5 +210,6 @@
       title: ipo.value.company_name,
       titleTemplate: title => `${title} - IPO Inbox`,
     })
+    console.log(ipo.value.promoters)
   })
 </script>
