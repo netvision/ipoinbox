cec99d796b0b7e2f684ec6de2fc5b4ca16f1c4ea
Rakesh Jangid <rakesh@jangid.co.in>
2022-03-02 13:28:32 +0530

    frontend ui trial

--- src/pages/Ipo.vue
+++ src/pages/Ipo.vue
@@ -10,14 +10,23 @@
       <div class="q-pa-sm flex q-gutter-sm bg-orange-3 rounded-borders">
         <span><a :href="ipo.company_url" target="_blank">{{ipo.company_url}}</a></span> <q-separator color="orange" vertical /> <a :href="nse.url">NSE</a> <q-separator color="orange" vertical /> <a :href="bse.url">BSE</a><q-separator color="orange" vertical /><span> Registrar: <a :href="registrar.url">{{registrar.name}}</a></span>
       </div>
-      <div class="row q-gutter-sm q-pa-md justify-between item-stretch">
-        <div class="rounded-border col-grow">
+      <div class="row q-gutter-sm q-pt-md justify-between item-stretch">
+        <div class="rounded-border col-12 col-md-4 bg-orange-2">
+          <q-item>
+            <q-item-section>
+              <q-item-label overline>CIN</q-item-label>
+              <q-item-label class="text-bold">{{curInfo.CIN}}</q-item-label>
+            </q-item-section>
+          </q-item>
           <q-item>
             <q-item-section>
               <q-item-label overline>Sector</q-item-label>
               <q-item-label class="text-bold">{{curInfo.SectorName}}</q-item-label>
             </q-item-section>
-            
+            <q-item-section>
+              <q-item-label overline>ISIN</q-item-label>
+              <q-item-label class="text-bold">{{curInfo.isin}}</q-item-label>
+            </q-item-section>
           </q-item>
           <q-item>
             <q-item-section>
@@ -31,16 +40,6 @@
           </q-item>
           <q-item>
             <q-item-section>
-              <q-item-label overline>CIN</q-item-label>
-              <q-item-label class="text-bold">{{curInfo.CIN}}</q-item-label>
-            </q-item-section>
-            <q-item-section>
-              <q-item-label overline>ISIN</q-item-label>
-              <q-item-label class="text-bold">{{curInfo.isin}}</q-item-label>
-            </q-item-section>
-          </q-item>
-          <q-item>
-            <q-item-section>
               <q-item-label overline>Market Cap</q-item-label>
               <q-item-label class="text-bold">&#8377;{{curInfo.MCAP}} Cr.</q-item-label>
             </q-item-section>
@@ -50,8 +49,7 @@
             </q-item-section>
           </q-item>
         </div>
-        <q-separator vertical spaced color="orange" />
-        <div class="col-grow">
+        <div class="col-12 col-md-grow bg-orange-2">
           <q-item>
             <q-item-section>
               <q-item-label overline>Listing Date</q-item-label>
@@ -101,8 +99,7 @@
             </q-item-section>
           </q-item>
         </div>
-       <q-separator vertical spaced color="orange" />
-        <div class="col-grow">
+        <div class="col-12 col-md-grow bg-orange-2">
           <q-item>
             <q-item-section>
               <q-item-label overline>EOD on</q-item-label>
@@ -162,31 +159,62 @@
     <div id="issue-detail">
       <q-card flat class="q-mt-md rounded-border bg-orange-1">
         <q-card-section>
-          <div class="text-h5">Issue Overview</div>
+          <div class="tw-text-2xl tw-font-serif tw-text-orange-500 tw-pb-4 tw-border-black">Issue Overview</div>
         </q-card-section>
         <q-card-section>
-          <div class="row">
-            <div class="q-pa-md col-4">
-                <q-item-label>Issue/Bid Opens on : <span class="text-bold">{{date.formatDate(ipo.open_date, 'dddd, Do MMMM, YYYY')}}</span></q-item-label>
-                <q-item-label>Issue/Bid Closes on : <span class="text-bold">{{date.formatDate(ipo.close_date, 'dddd, Do MMMM, YYYY')}}</span></q-item-label>
-                <q-item-label>Anchor Investors bids on : <span class="text-bold">{{date.formatDate(ipo.anchor_date, 'dddd, Do MMMM, YYYY')}}</span></q-item-label>
+          <div class="row q-gutter-sm">
+            <div class="q-pa-md col-12 col-md-grow rounded-border bg-orange-2">
+            
+                <q-item-label overline>Price Band</q-item-label>
+                <q-item-label><span class="text-bold">&#8377;{{ipo.price_band_low}} - &#8377;{{ipo.price_band_high}}</span></q-item-label>
+                <q-separator spaced color="orange" />
+                <q-item-label overline>Lot Size and Amount</q-item-label>
+                <q-item-label><span class="text-bold">{{ipo.lot_size}} Shares @ &#8377;{{ipo.lot_size * ipo.price_band_high}}</span></q-item-label>
+                <q-separator spaced color="orange" />
+                <q-item-label overline>Issue/Bid Opens on </q-item-label>
+                <q-item-label><span class="text-bold">{{date.formatDate(ipo.open_date, 'dddd, Do MMMM, YYYY')}}</span></q-item-label>
+                <q-separator spaced color="orange" />
+                <q-item-label overline>Issue/Bid Closes on</q-item-label>
+                <q-item-label><span class="text-bold">{{date.formatDate(ipo.close_date, 'dddd, Do MMMM, YYYY')}}</span></q-item-label>
+                <q-separator spaced color="orange" />
+                <q-item-label overline>Anchor Investors bids on </q-item-label>
+                <q-item-label><span class="text-bold">{{date.formatDate(ipo.anchor_date, 'dddd, Do MMMM, YYYY')}}</span></q-item-label>
+                <q-separator spaced color="orange" class="lt-md" />
             </div>
-            <q-separator vertical spaced color="orange" />
-            <div class="q-pa-md col">
-                <q-item-label>Price Band: <span class="text-bold">&#8377;{{ipo.price_band_low}} - &#8377;{{ipo.price_band_high}}</span></q-item-label>
-                <q-item-label class="text-italic">Lot Size: <span class="text-bold">{{ipo.lot_size}}</span></q-item-label>
-                <q-item-label class="text-italic">Application Amount: <span class="text-bold"> &#8377;{{ipo.lot_size * ipo.price_band_high}}</span></q-item-label>
+            <div class="q-pa-md col-12 col-md-grow rounded-border bg-orange-2">
+              <q-item-label overline>Issue Size</q-item-label>
+              <q-item-label><span class="text-bold">&#8377;{{formatNum((ipo.fresh_issue + ipo.offer_for_sale) * ipo.price_band_high)}}</span></q-item-label>
+              <q-separator spaced color="orange" />
+              <q-item-label overline>Fresh Issue</q-item-label>
+              <q-item-label><span class="text-bold">{{(ipo.fresh_issue) ? ipo.fresh_issue + ' Shares' : 'NA'}}</span></q-item-label>
+              <q-separator spaced color="orange" />
+              <q-item-label overline>Offer for Sale</q-item-label>
+              <q-item-label><span class="text-bold">{{(ipo.offer_for_sale) ? ipo.offer_for_sale+' Shares' : 'NA'}}</span></q-item-label>
+              <q-separator spaced color="orange" />
+              <q-item-label overline>Market Cap at the Time of IPO</q-item-label>
+              <q-item-label><span class="text-bold">&#8377; {{formatNum(ipo.no_of_total_shares * ipo.price_band_high)}}</span></q-item-label>
+              <q-separator spaced color="orange" />
+              <q-item-label overline>Registrar</q-item-label>
+              <q-item-label><span class="text-bold">{{ipo.registrar?.name}}</span></q-item-label>
+              <q-separator spaced color="orange" />
             </div>
-            <q-separator vertical spaced color="orange" />
-            <div class="q-pa-md col">
-              <q-item-label>Issue Size: <span class="text-bold">&#8377;{{ipo.issue_size}} Crore </span></q-item-label>
-              <q-item-label class="text-italic">Fresh Issue: <span class="text-bold">{{ipo.fresh_issue}}</span></q-item-label>
-              <q-item-label class="text-italic">Offer For Sale: <span class="text-bold">{{ipo.offer_for_sale}}</span></q-item-label>
-            </div>
-            <q-separator vertical spaced color="orange" />
-            <div class="q-pa-md col">
-              <q-item-label>Market Cap: <span class="text-bold">&#8377;{{ipo.issue_size}} Crore </span></q-item-label>
-              <q-item-label class="text-italic">Total Shares: <span class="text-bold">{{ipo.no_of_total_shares}}</span></q-item-label>
+            <div class="q-pa-md col-12 col-md-grow rounded-border bg-orange-2">
+              <q-item-label>Tentative Calander</q-item-label>
+              <q-item-label>Finalisation of Basis</q-item-label>
+              <q-item-label><span class="text-bold">{{date.formatDate(ipo.t_finalisation_of_basis, 'dddd, Do MMMM, YYYY')}}</span></q-item-label>
+              <q-separator spaced color="orange" />
+              <q-item-label>Initiation of Refunds</q-item-label>
+              <q-item-label><span class="text-bold">{{date.formatDate(ipo.t_initiation_of_refunds, 'dddd, Do MMMM, YYYY')}}</span></q-item-label>
+              <q-separator spaced color="orange" />
+              <q-item-label>Credit of Equity Shares</q-item-label>
+              <q-item-label><span class="text-bold">{{date.formatDate(ipo.t_credit_of_equity_shares, 'dddd, Do MMMM, YYYY')}}</span></q-item-label>
+              <q-separator spaced color="orange" />
+              <q-item-label>Commencement</q-item-label>
+              <q-item-label><span class="text-bold">{{date.formatDate(ipo.t_commencement, 'dddd, Do MMMM, YYYY')}}</span></q-item-label>
+              <q-separator spaced color="orange" />
+              <q-item-label>Anchor Unlocking</q-item-label>
+              <q-item-label><span class="text-bold">{{date.formatDate(ipo.t_anchor_unlocking, 'dddd, Do MMMM, YYYY')}}</span></q-item-label>
+              <q-separator spaced color="orange" />
             </div>
           </div>
         </q-card-section>
@@ -242,6 +270,16 @@
    console.log(ele)
    ele.scrollIntoView()
    */
+  }
+
+  const formatNum = (num) => {
+    if(num > 999 && num < 1000000){
+        return (num/1000).toFixed(1) + 'K'; // convert to K for number from > 1000 < 1 million 
+    }else if(num > 10000000){
+        return (num/10000000).toFixed(1) + ' Cr'; // convert to M for number from > 1 million 
+    }else if(num < 900){
+        return num; // if value < 1000, nothing to do
+    }
   }
   onMounted(async() => {
     ipo.value = await axios.get('https://droplet.netserve.in/ipos/'+ipo_id+'?expand=registrar,promoters,listings,subscriptions,sector').then(r => r.data)
